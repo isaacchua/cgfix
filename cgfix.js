756b08fd2c890a2eed8ed1cd2134df664053674b
Isaac Chua <isaacchua@outlook.com>
2023-05-15 13:11:40 +0800

    Rewrote dynamic gallery features.

--- cgfix.js
+++ cgfix.js
@@ -1,6 +1,8 @@
 // CGFix.js - https://github.com/isaacchua/cgfix
 let cgfix = {
-    removeBlankLines: true
+    removeBlankLines: true,
+    galleryEnableHideFilter: true,
+    galleryEnableSortByAltName: true
 };
 mr = (function (mr, $, window, document, cgfix){
     "use strict";
@@ -12,38 +14,46 @@ mr = (function (mr, $, window, document, cgfix){
         });
     }
 
-    // fix dynamic gallery
-    let masonry = $(".masonry");
-    if (masonry) {
-        let container = $(".masonry__container");
-        let hide = masonry.attr("data-hide-filter") === "hide";
-        let filter = masonry.attr("data-default-filter");
-        let fix = function(){
-            if (container) {
-                if (hide) {
-                    // hide filter
-                    masonry.find(".masonry-filter-container").css("display","none");
+    // enable dynamic gallery features
+    if (cgfix.galleryEnableHideFilter || cgfix.galleryEnableSortByAltName) {
+        mr.components.documentReady.push(function(){
+            $(".masonry").each(function(){
+                let masonry = $(this);
+                let hide = cgfix.galleryEnableHideFilter && masonry.attr("data-hide-filter") === "hide";
+                let sortData = masonry.attr("data-sort-alt").toLowerCase();
+                let sortAsc = sortData === "asc";
+                let sort = cgfix.galleryEnableSortByAltName && (sortAsc || sortData === "desc");
 
-                    // delete all elements / images that are not shown to save memory
-                    if (filter) {
+                // hide filter
+                if (hide) {
+                    masonry.find(".masonry-filter-container").css("display","none");
+                }
+
+                // process this per container
+                masonry.find(".masonry__container").each(function(){
+                    let container = $(this);
+
+                    // delete all elements / images that are not shown to save memory, if filter is hidden
+                    if (hide) {
+                        let filter = masonry.attr("data-default-filter");
                         container.isotope("remove",container.find("div[data-masonry-filter!='" + filter + "']"))
                             .isotope("layout");
                     }
-                }
 
-                // sort the images by their alt name
-                container.isotope({
-                        getSortData: {
-                            altName: item => $(item).find("img").attr("alt")
-                        },
-                        sortBy: ["altName"],
-                        sortAscending: true
-                    })
-                    .isotope("updateSortData").isotope();
-            }
-        }
-        
-        mr.components.windowLoad.push(fix);
+                    // sort the images by their alt name
+                    if (sort) {
+                        container.isotope({
+                                getSortData: {
+                                    altName: item => $(item).find("img").attr("alt")
+                                },
+                                sortBy: ["altName"],
+                                sortAscending: sortAsc
+                            })
+                            .isotope("updateSortData").isotope();
+                    }
+                });
+            });
+        });
     }
 
     return mr;
