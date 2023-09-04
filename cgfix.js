45d3472bc97ec2d6d0a3399d04bf6c99f7bed632
Isaac Chua <isaacchua@outlook.com>
2023-09-04 02:03:19 -0400

    Added login button feature.

--- cgfix.js
+++ cgfix.js
@@ -61,5 +61,28 @@ mr = (function (mr, $, window, document, cgfix){
         });
     }
 
+    // enable login button
+    if (cgfix.loginEnable) {
+        mr.components.documentReady.push(function() {
+            // check whether links to logged in page exist
+            let loggedInCheck = $("a[href='/".concat(cgfix.loginGroupId, "/", cgfix.loginCheckLoggedInPage, "/']"));
+
+            // if the logged in page doesn't exist, create login link
+            if (loggedInCheck.length === 0) {
+                let loginUrl = "/login_only?redirect=".concat(cgfix.loginGroupId,
+                    cgfix.loginRedirectPage == null ? "/" : "/".concat(cgfix.loginRedirectPage, "/"));
+                $(".section-cont__content-menu").append("<li class=\"dropdown hasNoSubMenu\"><span class=\"dropdown__trigger\"><a href=\""
+                    .concat(loginUrl, "\" aria-label=\"", cgfix.loginLinkText, "\">", cgfix.loginLinkText, "</a></span></li>"));
+                $("footer #menu-footer-menu").append("<li class=\"menu-item menu-item-type-custom menu-item-object-custom\"><a href=\""
+                    .concat(loginUrl, "\" aria-label=\"", cgfix.loginLinkText, "\"><span class=\"h6 type--uppercase\">", cgfix.loginLinkText, "</span></a></li>"));
+            }
+
+            // delete logged in, blank (i.e., no access), and broken link list items
+            let links = loggedInCheck.add("a[href='/".concat(cgfix.loginGroupId, "//']")).add("a[href='[CONTENT-FOOTER_MENU_URL]']");
+            let listItems = links.parent("li").add(links.parent("span").parent("li"));
+            listItems.remove();
+        });
+    }
+
     return mr;
 }(mr, jQuery, window, document, cgfix));
